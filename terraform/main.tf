64d8faa043424d51007d5874d22d7568be39029c
erasecorp@hotmail.com <javarushforthosewhoseek>
2019-10-21 21:06:18 +0300

    added firewall rule

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,7 @@ provider "google" {
   project = "infra-256612"
   region = "europe-west1"
 }
+
 resource "google_compute_instance" "app" {
   name = "reddit-app"
   machine_type = "g1-small"
@@ -23,4 +24,22 @@ resource "google_compute_instance" "app" {
  # использовать ephemeral IP для доступа из Интернет
   access_config {}
  }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+  # Название сети, в которой действует правило
+  network = "default"
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports = [
+      "9292"]
+  }
+  # Каким адресам разрешаем доступ
+  source_ranges = [
+    "0.0.0.0/0"]
+  # Правило применимо для инстансов с тегом …
+  target_tags = [
+    "reddit-app"]
 }
