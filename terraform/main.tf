a5f16c9ba1fa9dfb571748ebb209eb1ddd51fe66
erasecorp@hotmail.com <javarushforthosewhoseek>
2019-10-21 21:07:10 +0300

    added tag

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "google_compute_instance" "app" {
   name = "reddit-app"
   machine_type = "g1-small"
   zone = "europe-west1-b"
-  tags = ["puma-server"]
+  tags = ["reddit-app"]
   metadata = {
     ssh-keys = "appuser:${file("~/.ssh/appuser.pub")}"
   }
