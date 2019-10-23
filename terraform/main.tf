ce4796a8f1d21545dc7955e51e1c2d284d4b4c0f
ayudin <erasecorp@hotmail.com>
2019-10-23 16:17:34 +0300

    added new firewall rule resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,6 +46,15 @@ resource "google_compute_instance" "app" {
     script = "files/deploy.sh"
   }
 }
+resource "google_compute_firewall" "firewall_ssh" {
+ name = "default-allow-ssh"
+ network = "default"
+ allow {
+  protocol = "tcp"
+  ports = ["22"]
+ }
+ source_ranges = ["0.0.0.0/0"]
+}
 
 resource "google_compute_firewall" "firewall_puma" {
   name    = "allow-puma-default"
