56d0d7fe391e4d8bbe084764887f01e487db9158
ayudin <erasecorp@hotmail.com>
2019-10-23 17:35:32 +0300

    added new firewall rule resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -70,3 +70,7 @@ resource "google_compute_firewall" "firewall_puma" {
 # Правило применимо для инстансов с тегом …
   target_tags = ["reddit-app"]
 }
+
+resource "google_compute_address" "app_ip" {
+  name = "reddit-app-ip"
+}
