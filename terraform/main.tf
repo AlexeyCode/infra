36aa6a988bb7179681aeaa74bf6c423981cd7209
ayudin <erasecorp@hotmail.com>
2019-10-24 15:11:51 +0300

    added modules path

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,14 +4,14 @@ provider "google" {
 }
 
 module "app" {
-  source          = "modules/app"
+  source          = "./modules/app"
   public_key_path = var.public_key_path
   app_disk_image  = var.app_disk_image
   private_key_path = var.private_key_path
 }
 
 module "db" {
-  source          = "modules/db"
+  source          = "./modules/db"
   public_key_path = var.public_key_path
   db_disk_image   = var.db_disk_image
   private_key_path = var.private_key_path
