4eb8d6adf1c0250b5fd1d4daab45d410008f93be
ayudin <erasecorp@hotmail.com>
2019-10-24 13:20:03 +0300

    split data base and application config

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,7 +3,7 @@ variable project {
 }
 variable region {
   description = "Region"
-  default = "europe-west1"
+  default     = "europe-west1"
 }
 variable public_key_path {
   description = "Path to the public key used for ssh access"
@@ -13,4 +13,12 @@ variable disk_image {
 }
 variable private_key_path {
   description = "Private key"
+}
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-1571904793"
+}
+variable db_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-mongo-1571905137"
 }
